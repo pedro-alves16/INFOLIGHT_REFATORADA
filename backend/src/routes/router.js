0a994e66187180181d3549c76778429762f12121
Pedro Alves <pedroalves16.dev@gmail.com>
2026-04-01 18:55:15 -0300

    alterando arquivo de html de login

--- backend/src/routes/router.js
+++ backend/src/routes/router.js
@@ -9,9 +9,11 @@ const _dirname = path.dirname(_filename);
 const router = express.Router();
 
 router.get('/cadastro', renderCreatePage);
+
 router.get('/', (req, res) => {
     res.sendFile(path.resolve(_dirname, '../../../frontend/html/index.html'));
 })
+
 router.get('/users/login', renderLoginPage);
 
 export default router
