49f08058286c5dcd14e82db96c9343a765067aba
Pedro Alves <pedroalves16.dev@gmail.com>
2026-06-04 15:23:37 -0300

    correção de interação com o repositorio

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -2,12 +2,12 @@ import express from "express";
 import { dataSource } from "../config/dataSource.js";
 import { userTest, User, userSchema } from "../model/entities/userModel.js";
 
-const userRepository = dataSource.getRepository(userSchema);
-
 const userRouter = express.Router();
 
 //rota para criar usuario
 userRouter.post("/users/create", async (req, res) => {
+  const userRepository = dataSource.getRepository(userSchema);
+
   const user = await userRepository.create(req.body);
   const result = await userRepository.save(user);
   return res.send(result);
@@ -15,22 +15,30 @@ userRouter.post("/users/create", async (req, res) => {
 
 //rota para fazer o login conectando o usuario
 userRouter.post("/users/login", async (req, res) => {
+  const userRepository = dataSource.getRepository(userSchema);
+
   const userCredentials = req.body;
+
   const userFromDatabase = await userRepository.findOneBy({
     email: userCredentials.email,
   });
 
-  const canLoggin = false;
+  if (!userFromDatabase) {
+    return res.status(401).json({
+      error: "Usuário não encontrado",
+    });
+  }
+
   if (userCredentials.password === userFromDatabase.password) {
-    res.status(200).json({
+    return res.status(200).json({
       userName: userFromDatabase.userName,
       canLoggin: true,
     });
-  } else {
-    res.json({
-      error: "não autorizado!",
-    });
   }
+
+  return res.status(401).json({
+    error: "Senha incorreta",
+  });
 });
 
 userRouter.get("/users/dashboard", (req, res) => {
